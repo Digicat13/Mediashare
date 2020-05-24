4be3348afbd6b93ed3bf565e08a77d4c3861908d
Maryna Diuh <maradug13@gmail.com>
2020-05-24 14:33:59 +0300

    init

---
+++ lib/widgets/title_section.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget titleSection = Container (
+  padding: const EdgeInsets.all(32),
+  child: Row (
+    children : [
+      Icon(
+        Icons.favorite_border,
+        color
+      ) //Icon
+    ]
+  ) //Row
+)
